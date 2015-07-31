1590f47a99b18a46ce3592a18e12403a88b515df
Tom Bremer <tom@tbremer.com>
2015-07-31 02:26:54 -0500

    whitespace fixes

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -7,8 +7,10 @@ alias dev='cd /Volumes/Dev'
 # GIT ALIASES
 alias sta='git status'
 alias master='git checkout master && git fetch && git pull origin master'
+
 function push {
   git add .; git commit -m "$1"; git push;
 }
+
 
 echo "== ALIASES LOADED\n"
