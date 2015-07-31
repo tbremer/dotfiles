dd22e4ec960bf956de72e629bc175620f2ff1d88
Tom Bremer <tom@tbremer.com>
2015-07-31 02:26:17 -0500

    add push function

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -7,5 +7,8 @@ alias dev='cd /Volumes/Dev'
 # GIT ALIASES
 alias sta='git status'
 alias master='git checkout master && git fetch && git pull origin master'
+function push {
+  git add .; git commit -m "$1"; git push;
+}
 
 echo "== ALIASES LOADED\n"
