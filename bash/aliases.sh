0d02af5237139071661f367ad10144931373abbb
Tom Bremer <tom.bremer@asynchrony.com>
2015-08-14 15:02:06 -0500

    git graph

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -7,5 +7,6 @@ alias dev='cd /Volumes/Dev'
 # GIT ALIASES
 alias sta='git status'
 alias master='git checkout master && git fetch && git pull origin master'
+alias ggraph='git log --graph --oneline --decorate'
 
 echo "== ALIASES LOADED\n"
