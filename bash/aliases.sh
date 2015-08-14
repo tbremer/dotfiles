baf120f6758dd39fc0c53d957d70876e511388fd
Tom Bremer <tom.bremer@asynchrony.com>
2015-08-14 15:29:03 -0500

    add file view of git log

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -8,5 +8,6 @@ alias dev='cd /Volumes/Dev'
 alias sta='git status'
 alias master='git checkout master && git fetch && git pull origin master'
 alias ggraph='git log --graph --oneline --decorate'
+alias gitk="git log --graph --stat --pretty=format:'%C(yellow)%H%Creset%C(white) - %Creset%C(cyan)%ad%Creset%n''%C(cyan)%an::%Creset %C(white)%s%d%Creset %C(dim white)'"
 
 echo "== ALIASES LOADED\n"
