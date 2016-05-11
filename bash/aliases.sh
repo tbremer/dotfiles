1da03c0afd47b4aeeaab3c05e5092e66c9c7c6aa
Tom Bremer <tom@tbremer.com>
2016-05-11 12:25:56 -0500

    add processes command to alias, add firewatch bg, add editorconfig

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -12,7 +12,37 @@ alias ggraph='git log --graph --oneline --decorate'
 alias gcgraph="git log --graph --stat --pretty=format:'%C(yellow)%H%Creset%C(white) - %Creset%C(cyan)%ad%Creset%n''%C(cyan)%an::%Creset %C(white)%s%d%Creset %C(dim white)'"
 
 function push {
-  git add .; git commit -m "$1"; git push;
+	git add .; git commit -m "$1"; git push;
+}
+
+function processes {
+	COMMAND="ps -ax"
+
+	while [[ $# > 1 ]]
+	do
+	key="$1"
+
+	case $key in
+		-x|--except)
+		EXCEPT="$2"
+		shift # past argument
+		;;
+		*)
+						# unknown option
+		;;
+	esac
+	shift # past argument or value
+	done
+
+	if [ EXCEPT ]; then
+		COMMAND="$COMMAND | grep -v $EXCEPT"
+	fi
+
+	if [[ -n $1 ]]; then
+		COMMAND="$COMMAND | grep $1"
+	fi
+
+	eval $COMMAND
 }
 
 
