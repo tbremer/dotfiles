e83444cf3a05c4d06a376ccc3b97020baad77e95
Tom Bremer <tom@tbremer.com>
2017-03-31 18:04:29 -0500

    Atom init and styles

---
+++ Atom/init.js
@@ -0,0 +1,44 @@
+atom.commands.add(
+  'atom-workspace',
+  'theme:invert',
+  function inverter() {
+    const reg = /light/;
+    const active = atom.themes.getActiveThemeNames();
+    const newValue = active.map(val => {
+      if (reg.test(val)) {
+        return val.replace(reg, 'dark');
+      }
+
+      return val.replace('dark', 'light');
+    });
+
+    atom.notifications.addInfo('Switching theme…');
+    atom.config.set('core.themes', newValue);
+  }
+);
+
+try {
+  const less = window.require('less');
+  const el = document.createElement('style');
+  const path = require('path').resolve(__dirname, 'styles.less');
+
+  require('fs').readFile(path, (err, data) => {
+    if (err) throw err;
+
+    less.render(data.toString(), (err, css) => {
+      if (err) throw err;
+
+      el.setAttribute('source-path', path);
+      el.setAttribute('priority', '2');
+
+      el.appendChild(document.createTextNode(css));
+
+      document.querySelector('atom-styles').appendChild(el);
+    })
+
+  });
+}
+catch (err) {
+  console.warn('Error loading stylesheet');
+  console.error(err);
+}
