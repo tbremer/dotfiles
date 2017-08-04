296403b0b708aed3f551076956b2f6f1d35bfb4c
Tom Bremer <tom.bremer@asynchrony.com>
2017-08-04 12:49:53 -0500

    add git history rewriter

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -62,6 +62,29 @@ function gdiff {
 	done;
 }
 
+function resethistory {
+	echo -n 'Old Email: '
+	read OLD_EMAIL
+
+	echo -n 'Correct name: '
+	read CORRECT_NAME
+
+	echo -n 'Correct email: '
+	read CORRECT_EMAIL
+
+	git filter-branch --env-filter '
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+}
+
 echo
 echo "Aliases Loaded 🎉"
-
