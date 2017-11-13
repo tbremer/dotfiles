01721d75e8e80e481707e7d4706c2eaec1ff11a0
Tom Bremer <tom.bremer@asynchrony.com>
2017-11-13 09:51:39 -0600

    add docker kill-all script

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -89,5 +89,19 @@ function resethistory {
 	' --tag-name-filter cat -- --branches --tags
 }
 
+function dockerded {
+	# stop all containers
+	docker stop $(docker ps -a -q)
+
+	# remove all containers
+	docker rm $(docker ps -a -q)
+
+	# remove all images
+	docker rmi $(docker images -a -q)
+
+	# Remove all volumes
+	docker volume rm $(docker volume ls -f dangling=true -q)
+}
+
 echo
 echo "Aliases Loaded 🎉"
