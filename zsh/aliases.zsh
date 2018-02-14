cc1f92a85e528eb35cf5948b76e33419b6ab2439
Tom Bremer <tom@tbremer.com>
2018-02-14 11:02:03 -0600

    update processes command to take multiple exceptions, update dl alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,7 +2,8 @@
 alias l='ls -lah'
 alias resource='source ~/.zshrc'
 alias download='curl -O -J'
-alias dl='curl -O -J'
+#alias dl='curl -O -J'
+alias dl='download'
 
 # GIT ALIASES
 alias sta='git status'
@@ -12,12 +13,16 @@ alias gname='git rev-parse --abbrev-ref HEAD';
 alias ggraph='git log --graph --oneline --decorate'
 alias gcgraph="git log --graph --stat --pretty=format:'%C(yellow)%H%Creset%C(white) - %Creset%C(cyan)%ad%Creset%n''%C(cyan)%an::%Creset %C(white)%s%d%Creset %C(dim white)'"
 
+# Add an alias for `unadd`
+git config --global alias.unadd "reset HEAD"
+
 function push {
 	git add .; git commit -m "$1"; git push;
 }
 
 function processes {
 	COMMAND="ps -ax"
+	EXCEPTIONS=()
 
 	while [[ $# > 1 ]]
 	do
@@ -25,7 +30,8 @@ function processes {
 
 		case $key in
 			-x|--except)
-				EXCEPT="$2"
+				# build exceptions array
+				EXCEPTIONS+=("$2")
 				shift # past argument
 				;;
 			*)
@@ -35,17 +41,22 @@ function processes {
 		shift # past argument or value
 	done
 
-	if [ -n "$EXCEPT" ]; then
-		echo "the exception: $EXCEPT"
-		COMMAND="$COMMAND | grep -v $EXCEPT"
+	# if exceptions exist build the command up
+	if [ ${#EXCEPTIONS[@]} -gt 0 ]; then
+		for EXCEPT in "${EXCEPTIONS[@]}"; do
+			COMMAND="$COMMAND | grep -v $EXCEPT"
+		done
 	fi
 
+	# if we still have arguments these are our search string, add it to our
+	# command
 	if [[ -n $1 ]]; then
 		COMMAND="$COMMAND | grep $1"
 	fi
 
-	echo "the command: $COMMAND"
+	#echo "the command: $COMMAND"
 
+	# run the command
 	eval $COMMAND
 }
 
