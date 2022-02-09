e82b04a4d29fed5e05f80fbfd8aeb2d3f02a2552
Tom Bremer <tom@tbremer.com>
2022-02-09 10:39:30 -0600

    rename cleanhouse to cleandesk

--- fish/functions/cleandesk.fish
+++ fish/functions/cleandesk.fish
@@ -1,4 +1,4 @@
-function cleanhouse
+function cleandesk
   set exists (defaults read com.apple.finder CreateDesktop)
   if test "$exists" = "false"
     defaults write com.apple.finder CreateDesktop true
