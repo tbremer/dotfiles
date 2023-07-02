e60f31f41aa1a03c02d0a8bd8b85d4f697481592
Tom Bremer <tom@tbremer.com>
2023-07-02 18:20:53 -0500

    update-appearance script tweak

--- bash/update-appearance.sh
+++ bash/update-appearance.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-[[ $(defaults read -g AppleInterfaceStyle 2>/dev/null) == "Dark" ]] && echo "Default" > $HOME/.appearance || echo "Light" > $HOME/.appearance
+[[ $(defaults read -g AppleInterfaceStyle 2>/dev/null) == "Dark" ]] && echo "Dark" > $HOME/.appearance || echo "Light" > $HOME/.appearance
