430aedda925b3bc2a91359867e2597ba8a7eb013
Tom Bremer <tom@tbremer.com>
2024-01-19 20:10:20 -0600

    turn off formatters that aren't prettier

--- astro-vim/init.lua
+++ astro-vim/init.lua
@@ -42,6 +42,9 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
+        "tsserver",
+        "eslint",
+        "eslint_d",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
